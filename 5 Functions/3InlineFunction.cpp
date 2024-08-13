e082f197dfa56c992ecf0a8eb0ede0a71aa1029e
Uditya <uditya204@gmail.com>
2024-08-13 11:16:47 +0530

    changes

--- 5 Functions/3InlineFunction.cpp
+++ 5 Functions/3InlineFunction.cpp
@@ -1,3 +1,9 @@
+/*
+When a function is inline , then the complete body of the function is pasted in place of function call statement at the time of execution , reducing the time spent on going to the function and coming back to the call statement . Thus reducing overhead.
+
+- If the function is small( approx 2 lines), then we can make that function inline and save some execution delay.
+- If the function is long and we make that function inline then the compiler will rejet our request of inline and will use it as a normal function.
+*/
 #include <iostream>
 using namespace std;
 
