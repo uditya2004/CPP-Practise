54122d3034f0389ea9e7c62e09e9e9f4dda0ebae
Uditya <uditya204@gmail.com>
2024-08-13 09:32:21 +0530

    changes

--- 6 OPPS/6Destructor.cpp
+++ 6 OPPS/6Destructor.cpp
@@ -1,33 +1,58 @@
 #include <iostream>
 using namespace std;
 
-class Car{
-public:
+//Eample 1
+class Student {
     string name;
-    string color ;
-    int *mileage;
+    string regno;
 
-    Car(string name, string color) {
-        this ->name = name;
-        this ->color = color;
-        *mileage = 12;
+public:
+    Student(string nameVal, string regnoVal){
+        name = nameVal;
+        regno = regnoVal;
     }
 
-    ~Car() {
-        cout<<"deleting object.." <<endl;
-        if (mileage != NULL) {
-            delete mileage;
-            mileage = NULL;
-        }
+    ~Student(){
+        cout<<"Destructor called....";
     }
 };
 
 int main() {
-    Car obj1("BMW", "Blue");
-    
-    cout << "name:- "<<obj1.name<<endl;
-    cout << "color:- "<<obj1.color<<endl;
-    cout << "mileage:- "<<*obj1.mileage<<endl;
+    Student stud1("uditya", "23bce10497");
 
-    return 0;
+
 }
+
+//--------------------------------
+//Example 2
+// class Car{
+// public:
+//     string name;
+//     string color ;
+//     int *mileage;
+
+//     Car(string name, string color) {
+//         this ->name = name;
+//         this ->color = color;
+//         mileage = new int;  // Allocate memory for mileage
+//         *mileage = 12;
+//     }
+
+//     ~Car() {
+//         cout<<"deleting object.." <<endl;
+//         if (mileage != NULL) {
+//             delete mileage;
+//             mileage = NULL;
+//         }
+//     }
+// };
+
+// int main() {
+//     Car obj1("BMW", "Blue");
+    
+//     cout << "name:- "<<obj1.name<<endl;
+//     cout << "color:- "<<obj1.color<<endl;
+//     cout << "mileage:- "<<*obj1.mileage<<endl;
+
+//     return 0;
+// }
