637ce6652412853d223099d3f4734c07e26ec003
Uditya <uditya204@gmail.com>
2024-08-26 22:23:12 +0530

    changes

--- 9 Working with files/2ReadFromFile.cpp
+++ 9 Working with files/2ReadFromFile.cpp
@@ -67,3 +67,51 @@ int main() {
 
     return 0;
 }
+
+
+
+//===============================
+//Question: WAP to write 3 string into the file and read all 3 string from the File
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    string write_data;
+
+    //Writing Into the file 
+    ofstream obj1;
+    obj1.open("practise.txt");
+
+    for (int i=1 ; i<4 ; i++){
+        cout<<"Enter "<<i<<" string";
+        getline(cin, write_data);
+
+        obj1<<write_data<<"\n";
+
+    }
+
+    obj1.close();  //Close the file before you start reading it , as This can cause problems because the data might not be fully flushed to the file before you try to read it.
+
+
+    //Reading from the file (every line)
+    cout<<"\nReading the file..."<<endl;
+    
+    ifstream obj2;
+    obj2.open("practise.txt");
+
+    string read_data;
+
+    while (obj2.eof() == false) {
+        getline(obj2, read_data );
+
+        cout<<read_data<<endl;
+    }
+
+    
+    obj2.close();
+
+    return 0;
+}
--- Test.cpp
+++ Test.cpp
@@ -4,19 +4,39 @@
 using namespace std;
 
 int main(){
-    string data;
+    string write_data;
 
+    //Writing Into the file 
     ofstream obj1;
     obj1.open("practise.txt");
 
     for (int i=1 ; i<4 ; i++){
         cout<<"Enter "<<i<<" string";
-        getline(cin, data);
+        getline(cin, write_data);
 
-        obj1<<data<<"\n";
+        obj1<<write_data<<"\n";
 
     }
 
+    obj1.close();  //Close the file before you start reading it , as This can cause problems because the data might not be fully flushed to the file before you try to read it.
+
+
+    //Reading from the file (every line)
+    cout<<"\nReading the file..."<<endl;
+    
+    ifstream obj2;
+    obj2.open("practise.txt");
+
+    string read_data;
+
+    while (obj2.eof() == false) {
+        getline(obj2, read_data );
+
+        cout<<read_data<<endl;
+    }
+
+    
+    obj2.close();
 
     return 0;
 }
